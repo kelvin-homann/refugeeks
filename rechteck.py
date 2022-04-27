c80919df3f8a3cac4fcaa00344ac5cf6e6400dde
Kelvin Homann <khomann@frobese.com>
2022-04-27 14:47:48 +0200

    properly added string

--- rechteck.py
+++ rechteck.py
@@ -3,9 +3,8 @@
 # Dazu soll erst die Anzahl der Zahlen eingegeben werden, im Anschluss werden entsprechend viel
 # Zahlen eingegeben und der der Durchschnitt angezeigt. Ein möglicher Ablauf ist wie folgt:
 
-
-x = int(input("Breite: "))
-y = int(input("Tiefe: "))
+x = int(input("Bitte gib eine Breite an: "))
+y = int(input("Bitte gib eine Tiefe an: "))
 
 s = "*"
 for i in range(y):
