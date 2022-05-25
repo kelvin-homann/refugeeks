c45c6fc4e1b72da6ab5f5cbb2f46dd762b153ad1
Kelvin Homann <kelvin.homann@gmail.com>
2022-05-25 13:53:31 +0200

    add quicksort

--- python_3/00_bubble_sort.py
+++ python_3/00_bubble_sort.py
@@ -1,11 +1,28 @@
-def bubbleSort(array):
-    n = len(array)
+import time
+import random
+
+
+def bubbleSort(my_list):
+    n = len(my_list)
+
     for i in range(n - 1):
+        print(n, i, n - i - 1)
         for j in range(0, n - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
 
 
-my_list = [5, 3, 1, 8, 10]
+my_list = [2, 25, 12, 52, 10]
+# my_list = random.sample(range(0, 9999), 8800)
+# my_list_new = my_list
+
+# startBubble = time.time()
 bubbleSort(my_list)
-print(my_list)
+# endBubble = time.time()
+
+# startPython = time.time()
+# my_list_new.sort()
+# endPython = time.time()
+
+# print("{} {}".format((endBubble-startBubble)
+#       * 1000, (endPython-startPython) * 1000))
---
+++ python_3/00_quicksort.py
@@ -0,0 +1,30 @@
+def quick_sort(start, end, my_list):
+    if (start < end):
+        p = partition(start, end, my_list)
+
+        quick_sort(start, p - 1, my_list)
+        quick_sort(p + 1, end, my_list)
+
+
+def partition(start, end, my_list):
+    pivot_index = start
+    pivot = my_list[pivot_index]
+
+    while start < end:
+        while start < len(my_list) and my_list[start] <= pivot:
+            start += 1
+
+        while my_list[end] > pivot:
+            end -= 1
+
+        if (start < end):
+            my_list[start], my_list[end] = my_list[end], my_list[start]
+
+    my_list[end], my_list[pivot_index] = my_list[pivot_index], my_list[end]
+
+    return end
+
+
+my_list = [2, 25, 12, 52, 10]
+quick_sort(0, len(my_list) - 1, my_list)
+print(my_list)
