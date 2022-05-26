553029522d16964e17e31742d6618f523f1efbe2
Kelvin Homann <kelvin.homann@gmail.com>
2022-05-26 11:33:03 +0200

    add numpy and sort() to comparison

--- python_3/sorting_times_vis/diagram.py
+++ python_3/sorting_times_vis/diagram.py
@@ -10,21 +10,28 @@ from quicksort import quick_sort
 class Diagram:
     def __init__(self):
         self.list_length_steps = [10, 100, 1000, 2000,
-                                 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000] 
+                                  3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 
         self.fig, self.axis = plt.subplots()
         self.axis.set(xlabel='Listenlänge (n)', ylabel='Laufzeit (ms)',
-                      title='BubbleSort vs Mergesort')
+                      title='Vergleich von Sortier-Methoden')
         self.axis.set_xticks(self.list_length_steps)
 
         self.randomListsMergeSort = self.generateRandomLists()
-        self.randomListsBubbleSort = self.generateRandomLists()
-        self.randomListsQuickSort = self.generateRandomLists()
-
         self.mergeSortTimes = self.measureMergeSortTimes()
+
+        self.randomListsQuickSort = self.generateRandomLists()
         self.quickSortTimes = self.measureQuickSortTimes()
+
+        self.randomListsPythonSort = self.generateRandomLists()
+        self.pythonSortTimes = self.measurePythonSortTimes()
+        print(self.pythonSortTimes)
+
+        self.randomListsNumpySort = self.generateRandomLists()
+        self.numpySortTimes = self.measureNumpySortTimes()
+
+        self.randomListsBubbleSort = self.generateRandomLists()
         self.bubbleSortTimes = self.measureBubbleSortTimes()
-        # self.ax.set_yticks(self.bubbleSortTimes)
 
     def drawLogCurve(self):
         # O(n * log(n))
@@ -84,6 +91,29 @@ class Diagram:
 
         return times
 
+    def measurePythonSortTimes(self):
+        times = []
+
+        for l in self.randomListsPythonSort:
+            begin = time.time()
+            l.sort()
+            end = time.time()
+            times.append((end-begin) * 1000)
+
+        return times
+
+    def measureNumpySortTimes(self):
+        times = []
+
+        for l in self.randomListsNumpySort:
+            np.asarray(l)  # convert python list to numpy array
+            begin = time.time()
+            np.sort(l)
+            end = time.time()
+            times.append((end-begin) * 1000)
+
+        return times
+
     def drawScatterMergeSort(self):
         self.axis.scatter(self.list_length_steps, self.mergeSortTimes, vmin=0, vmax=self.mergeSortTimes[len(
             self.mergeSortTimes) - 1], c='#d62728', label="Mergesort")
@@ -96,14 +126,23 @@ class Diagram:
         self.axis.scatter(self.list_length_steps, self.quickSortTimes, vmin=0, vmax=self.quickSortTimes[len(
             self.quickSortTimes) - 1], c='#032cfc', label="Quicksort")
 
+    def drawScatterPythonSort(self):
+        self.axis.scatter(self.list_length_steps, self.pythonSortTimes, vmin=0, vmax=self.pythonSortTimes[len(
+            self.pythonSortTimes) - 1], c='#eb9234', label="Python sort()")
+
+    def drawScatterNumpySort(self):
+        self.axis.scatter(self.list_length_steps, self.numpySortTimes, vmin=0, vmax=self.numpySortTimes[len(
+            self.numpySortTimes) - 1], c='#34ebd2', label="Numpy Sort")
+
     def draw(self):
         self.drawScatterMergeSort()
         self.drawScatterBubbleSort()
         self.drawScatterQuickSort()
+        self.drawScatterPythonSort()
+        self.drawScatterNumpySort()
         # self.drawLogCurve()
         # self.drawExponentialCurve()
         self.axis.legend()
-        # plt.xlim(0, self.bubbleSortTimes[0])
         plt.show()
 
 
