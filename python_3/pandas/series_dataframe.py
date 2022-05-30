a4e4df2d54cf8046c8e29ea6fca6c3723b6f7d7b
Kelvin Homann <kelvin.homann@gmail.com>
2022-05-30 16:22:05 +0200

    add csv

---
+++ python_3/pandas/series_dataframe.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+temperatures = pd.Series(np.random.randint(1, 50, size=5), [
+                         "DE", "FR", "CH", "ES", "PO"], dtype=np.int32)
+
+# print(temperatures.describe())
+print(temperatures)
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Kelvin",
+            "Julia"
+        ],
+        "Age": np.random.randint(18, 67, size=2),
+        "Sex": ["male", "female"],
+        "Height": np.random.randint(140, 200, size=2)
+    }
+)
+
+# print(df.describe())
+
+print(df)
